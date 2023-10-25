495557722dc6ea6cb9213f9e9bc70cf2b18a25fc
M.can <caglayan.mertcan37@gmail.com>
2023-10-25 22:24:47 +0300

    style updated, localStorage added, newbutton added

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 let formElement = document.querySelector("#form");
 let textElement = document.querySelector("#note-text");
 let cardsContainer = document.querySelector(".cards-container");
+let clearBtnElement = document.querySelector("#clear-button")
+
+function updateStroge() { 
+    localStorage.setItem("notes",cardsContainer.innerHTML)
+ }
 
 formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+	event.preventDefault();
 	let note = textElement.value;
-    let newNote = document.createElement("div");
-    newNote.classList.add("card");
-    newNote.innerHTML = `<p>${note}</p><i class="fa-solid fa-trash"></i>`
-    cardsContainer.appendChild(newNote);
-
+	let newNote = document.createElement("div");
+	newNote.classList.add("card");
+	newNote.innerHTML = `<p>${note}</p><i class="fa-solid fa-trash"></i>`;
+	cardsContainer.prepend(newNote);
+    textElement.value = ""
+    updateStroge()
+    
     newNote.addEventListener("click", () => {
-        let trashIcon = newNote.querySelector("i.fa-solid.fa-trash");
-        if (newNote.classList.contains("grow")) {
-            newNote.classList.remove("grow");
-            trashIcon.style.display = "none";
-            newNote.style.overflow = "hidden";
-            newNote.style.height = "50px";
-        } else {
-            newNote.classList.add("grow");
-            newNote.style.height = "auto";
-            trashIcon.style.display = "inline";
-            trashIcon.addEventListener("click", () => {
-                newNote.remove();
-            });
-        }
+        growUp(newNote);
+    });
+
+});
+
+
+
+function growUp(note) {
+	let trashIcon = note.querySelector("i.fa-solid.fa-trash");
+	if (note.classList.contains("grow")) {
+		note.classList.remove("grow");
+		trashIcon.style.display = "none";
+		note.style.overflow = "hidden";
+		note.style.height = "50px";
+	} else {
+		note.classList.add("grow");
+		note.style.height = "auto";
+		trashIcon.style.display = "inline";
+		trashIcon.addEventListener("click", () => {
+			note.remove();
+            updateStroge()
+		});
+	}
+}
+
+if (localStorage.getItem("notes")) {
+    cardsContainer.innerHTML = localStorage.getItem("notes");
+}
+
+cardsContainer.querySelectorAll('.card').forEach((note) => {
+    note.addEventListener('click', () => {
+        growUp(note);
     });
 });
+
+clearBtnElement.addEventListener("click", () => {
+    cardsContainer.innerHTML = '';
+    updateStroge()
+})
